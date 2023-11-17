5e23fc1d590d03a922a622c01c067718f74ebd1f
KYJ\span5 <span5189@naver.com>
2023-11-17 23:29:40 +0900

    테이블 addr 주석처리

--- keus_prj/member/admin.py
+++ keus_prj/member/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from django.db import models
+from .models import Board, Member, Category
 # Register your models here.
+
+admin.site.register(Board)
+admin.site.register(Member)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('name',)}
+    # prepopulated_fields : 장고 제공 기능,
+    # 다른 필드의 값을 가지고 와서 자동으로 채울수 있도록 함
+
+
+admin.site.register(Category, CategoryAdmin)
