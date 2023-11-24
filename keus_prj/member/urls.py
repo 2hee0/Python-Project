42101f481bb1b4d1d02592143475d0c2b12d0ea7
[jiheon] <wlgjs83@gmail.com>
2023-11-24 17:47:57 +0900

    회원정보 찾기 기능 구현완료

--- keus_prj/member/urls.py
+++ keus_prj/member/urls.py
@@ -6,4 +6,7 @@ urlpatterns=[
     # localhost:8000/blog 매핑
     path('join/',views.join, name='join'),
     path('login/', views.login, name='login'),
+    path('header/', views.header, name='header'),
+    path('find/', views.find, name='find'),
+    path('check_username/', views.check_username, name='check_username'),
 ]
--- keus_prj/member/views.py
+++ keus_prj/member/views.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -37,9 +38,55 @@ def login(request):
             # 로그인 처리
             member = members.first()  # 검색된 회원중 첫번째 회원을 가져옴
             request.session['username'] = member.username
-            return redirect('/')
+            return redirect(reverse('header'))
         else:
             # 로그인 실패
             return render(request, 'member/login.html', {'error_message': '아이디 또는 비밀번호를 잘못 입력했습니다.'})
 
     return render(request, 'member/login.html')
+
+# 로그인 성공시 메인페이지로 이동(지금은 임시로 member/header.html 로 )
+def header(request):
+    return render(request, 'member/header.html')
+
+# 아이디 및 비밀번호 찾기를 위한 find 함수 생성
+def find(request):
+    if request.method == 'GET':
+        find_type = request.GET.get('type')  # type은 아이디 찾기인지 비밀번호 찾기인지 구분하는 매개변수 -> HTML hidden 지정
+        if find_type == 'username':
+            email = request.GET.get('email', '')
+            Tell = request.GET.get('Tell', '')
+
+            try:
+                member = Member.objects.get(email=email, Tell=Tell)
+                return JsonResponse({'username': member.username})  # 아이디를 JSON 형태로 응답
+
+            except ObjectDoesNotExist:
+                return JsonResponse({'error': '사용자를 찾을 수 없습니다.'})  # 사용자를 찾지 못한 경우 에러 응답
+
+        elif find_type == 'password':
+            username = request.GET.get('username', '')
+            email = request.GET.get('email', '')
+            Tell = request.GET.get('Tell', '')
+
+            try:
+                member = Member.objects.get(username=username, email=email, Tell=Tell)
+                return JsonResponse({'password': member.password})
+
+            except ObjectDoesNotExist:
+                return JsonResponse({'error': '비밀번호를 찾을 수 없습니다.'})
+
+        return render(request, 'member/find.html')
+
+def check_username(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+
+        try:
+            existing_member = Member.objects.get(username=username)
+            return JsonResponse({'message': '이미 존재하는 아이디입니다.', 'status': 'error'})
+
+        except Member.DoesNotExist:
+            return JsonResponse({'message': '사용 가능한 아이디입니다.', 'status': 'success'})
+
+    return JsonResponse({'message': '잘못된 요청입니다.', 'status': 'error'})
